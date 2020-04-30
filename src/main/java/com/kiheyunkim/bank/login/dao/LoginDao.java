08bbe4b82e89a6ce9a63f65720fa062d27ecd3c2
kiheyunkim <kiheyunKim@gmail.com>
2020-04-30 17:37:26 +0900

    add Login Logic

--- src/main/java/com/kiheyunkim/bank/login/dao/LoginDao.java
+++ src/main/java/com/kiheyunkim/bank/login/dao/LoginDao.java
@@ -1,7 +1,12 @@
 package com.kiheyunkim.bank.login.dao;
 
+import java.util.Date;
+
 public interface LoginDao {
-	public void addUser(String id, String pw);
+	public Boolean addUser(String id, String pw, String salt,Date lastChange, Boolean isVerified);
+	public Boolean modifyPassword(String id, String pw, String newPw, String newSalt);
+	
+	public void addUser(String id, String pw, String salt);
 	public Boolean checkLogin(String id, String pw);
 	public void renewPassword(String id, String prevPw, String newPw);
 }
--- src/main/java/com/kiheyunkim/bank/login/service/LoginService.java
+++ src/main/java/com/kiheyunkim/bank/login/service/LoginService.java
@@ -2,11 +2,21 @@ package com.kiheyunkim.bank.login.service;
 
 import org.springframework.stereotype.Service;
 
+import com.kiheyunkim.bank.login.dao.LoginDao;
+
 @Service
 public class LoginService {
 	
-	public Boolean loginCheck(String id, String pw) {
+	private LoginDao loginDao;
+	
+	public LoginService(LoginDao loginDao) {
+		this.loginDao = loginDao;
+	}
+	
+	public Boolean loginCheck(String id, String pw) {		
 		return true;
 	}
+	
+	public 
 	
 }
