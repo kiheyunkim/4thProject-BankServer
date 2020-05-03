58a44410837441a2d483835126207a051097b133
kiheyunkim <kiheyunKim@gmail.com>
2020-05-03 18:47:22 +0900

    Modify Models

---
+++ src/main/java/com/kiheyunkim/bank/account/dao/AccountDao.java
@@ -0,0 +1,11 @@
+package com.kiheyunkim.bank.account.dao;
+
+import com.kiheyunkim.bank.account.model.AccountModel;
+import com.kiheyunkim.bank.account.model.AccountType;
+
+public interface AccountDao {
+	public String addAccount(String password, AccountType type);
+	public AccountModel getAccount(long accountNum);
+	public void updateAccount(AccountModel accountModel);
+	public void deleteACcount(long accountNum);	
+}
---
+++ src/main/java/com/kiheyunkim/bank/account/dao/AccountDaoImp.java
@@ -0,0 +1,48 @@
+package com.kiheyunkim.bank.account.dao;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+import com.kiheyunkim.bank.account.model.AccountModel;
+import com.kiheyunkim.bank.account.model.AccountType;
+
+public class AccountDaoImp implements AccountDao{
+
+	private final SessionFactory sessionFactory;
+	
+	public AccountDaoImp(SessionFactory sessionFactory) {
+		this.sessionFactory = sessionFactory; 
+	}
+	
+	@Override
+	public String addAccount(String password, AccountType type) {
+		Session session = sessionFactory.getCurrentSession();
+		AccountModel accountModel = new AccountModel(password, type);
+		
+		session.persist(accountModel);
+		
+		return String.format("%011d", accountModel.getAccountNum());
+	}
+
+	@Override
+	public AccountModel getAccount(long accountNum) {
+		Session session = sessionFactory.getCurrentSession();
+		AccountModel accountModel = session.get(AccountModel.class, String.format("%011d", accountNum));
+		
+		return accountModel;
+	}
+
+	@Override
+	public void updateAccount(AccountModel accountModel) {
+		Session session = sessionFactory.getCurrentSession();
+		session.update(accountModel);
+	}
+
+	@Override
+	public void deleteACcount(long accountNum) {
+		Session session = sessionFactory.getCurrentSession();
+		AccountModel find = session.find(AccountModel.class, accountNum);
+		
+		session.delete(find);
+	}
+}
